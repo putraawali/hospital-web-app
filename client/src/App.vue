4cdfeeb976b28e513f74401dadb8630028e6bd9f
Riski Putra Awali <riskiputra270@gmail.com>
2021-06-04 23:25:29 +0700

    add repo

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+<script>
+  import Footer from "./components/Footer.vue";
+  import Navbar from "./components/Navbar.vue";
+  export default {
+    name: "App",
+    components: {
+      Footer,
+      Navbar,
+    },
+  };
+</script>
+<style>
+  .action-box {
+    color: inherit;
+  }
+  .action-box:hover {
+    color: brown;
+    cursor: pointer;
+  }
+</style>
